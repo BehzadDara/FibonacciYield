f182a7f19eb4a9859ba1c17ccccd6ea17bee2d87
behzad dara <Behzad.dara.99@gmail.com>
2024-03-29 20:26:06 +0330

    Add project files.

---
+++ FibonacciYield/Program.cs
@@ -0,0 +1,21 @@
+﻿foreach (var number in GenerateFibonacciSequence())
+{
+    if (number > 100)
+        break;
+
+    Console.WriteLine(number);
+}
+
+static IEnumerable<int> GenerateFibonacciSequence()
+{
+    int a = 0, b = 1;
+
+    while (true)
+    {
+        // Yield the current Fibonacci number
+        yield return a;
+
+        // Calculate the next Fibonacci number
+        (a,b) = (b, a + b);
+    }
+}
